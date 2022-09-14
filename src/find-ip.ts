7c74f2a79545786c6c8d084035b6af0d4941cdc6
akejolin <akejolin@gmail.com>
2022-09-15 01:21:55 +0200

    improved ip regex

--- src/find-ip.ts
+++ src/find-ip.ts
@@ -1,4 +1,5 @@
-export const findIp = (blob:string):string[]|null =>
-  String(blob).match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/g)  
+import ipRegex from './ip-regex'
+
+export const findIp = (blob:string):string[]|null => String(blob).match(ipRegex()) 
 
 export default findIp
